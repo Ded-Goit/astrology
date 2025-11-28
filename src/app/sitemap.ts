cfe5f3b480f4010ceb296ba59c6b26819083aca5
Andriy Romanov <mgm.agro04@gmail.com>
2025-11-28 17:08:32 +0200

    added seo

---
+++ src/app/sitemap.ts
@@ -0,0 +1,38 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://astrology-azure-zeta.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+  ];
+}
+/** export default function sitemap() {
+  return [
+    {
+      url: "https://astrology-azure-zeta.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+    {
+      url: "https://astrology-azure-zeta.vercel.app/services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://astrology-azure-zeta.vercel.app/about",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: "https://astrology-azure-zeta.vercel.app/contact",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+  ];
+}*/
